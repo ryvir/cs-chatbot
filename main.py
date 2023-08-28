78a015be1d76e720ff06bdd6d0e09e8de020a630
ryv10 <74744128+ryvir@users.noreply.github.com>
2023-08-28 11:32:40 -0700

    Chatbot works now

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import json
+import json, time
 
 trooms = {}
 with open("teacher list.json", 'r') as jsonf:
   trooms = json.load(jsonf)
 teachers = list(trooms)
-
 
 def checkInput(inputType):
   
@@ -12,17 +11,15 @@ def checkInput(inputType):
     q2 = input("Which teacher do you want to find? ")
     for i in q2.split():
       if i in teachers:
-        print(trooms[i])
+        print(trooms[i])  
+        time.sleep(2)
       else:
         pass
   elif int(inputType) == 2:
-		awesomeInput = input('Which letter building do you need the location of? Use the format "Q Building"')
-		if "R" in awesomeInput:
-			
-  elif int(inputType) == 3:
+    q2 = input("Which building do you want to find teachers in? Just type the letter. ")
     for teacher in list(trooms):
-      if trooms[teacher][0] == q2.lower():
-        print(teacher)
+      if "-" in trooms[teacher] and trooms[teacher][0] == q2.lower():
+        print(teacher.capitalize())
 
 
 print(
@@ -33,8 +30,7 @@ while True:
   # print("Ask me a question about a building on campus!")
   print("Type the number of the type of question you would like to ask.\n")
   print("1. Which room a teacher is in.")
-  print("2. Where a building is.")
-  print("3. What teachers are in a building.")
+  print("2. What teachers are in a building.")
   userInput = input(
       "Type the number. Type 'q' to quit, or type 'h' for help. \n")
   if (userInput == "h"):
